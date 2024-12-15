952010a4d996ff3e3549c3f94b76034d3a548ea0
Revess <bjmaat@gmail.com>
2024-12-15 20:09:58 +0100

    Added some workflow details

--- plugins/MusicGen/build.sh
+++ plugins/MusicGen/build.sh
@@ -6,8 +6,14 @@ export PKG_CONFIG_PATH="/opt/homebrew/opt/qt/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 if [ "$UNAME_S" = "Linux" ]; then
     make 
+    sudo rm -rf ../../vst2/MusicGenVST.vst
+    sudo cp -r ../../bin/MusicGenVST.vst ../../vst2/MusicGenVST.vst
+    rm -rf ../../vst3/MusicGenVST.vst3
     sudo cp -r ../../bin/MusicGenVST.vst3 ../../vst3/MusicGenVST.vst3
     ../../bin/MusicGenVST
+    zip -r ../../release/standalone-x86_64-linux.zip ../../bin/MusicGenVST.app
+    zip -r ../../release/vst2-x86_64-linux.zip ../../bin/MusicGenVST.vst
+    zip -r ../../release/vst3-x86_64-linux.zip ../../bin/MusicGenVST.vst3
 elif [ "$UNAME_S" = "Darwin" ]; then
     if [ "$UNAME_M" = "x86_64" ]; then
         arch -x86_64 make
@@ -19,6 +25,9 @@ elif [ "$UNAME_S" = "Darwin" ]; then
         rm -rf ../../vst3/MusicGenVST.vst3
         cp -r ../../bin/MusicGenVST.vst3 ../../vst3/MusicGenVST.vst3
         arch -x86_64 ../../bin/MusicGenVST.app/Contents/MacOS/MusicGenVST
+        zip -r ../../release/standalone-x86_64-osx.zip ../../bin/MusicGenVST.app
+        zip -r ../../release/vst2-x86_64-osx.zip ../../bin/MusicGenVST.vst
+        zip -r ../../release/vst3-x86_64-osx.zip ../../bin/MusicGenVST.vst3
     elif [ "$UNAME_M" = "arm64" ]; then
         arch -arm64 make
         echo "Running on macOS arm64"
@@ -29,6 +38,9 @@ elif [ "$UNAME_S" = "Darwin" ]; then
         rm -rf ../../vst3/MusicGenVST.vst3
         cp -r ../../bin/MusicGenVST.vst3 ../../vst3/MusicGenVST.vst3
         arch -arm64 ../../bin/MusicGenVST.app/Contents/MacOS/MusicGenVST
+        zip -r ../../release/standalone-arm64-osx.zip ../../bin/MusicGenVST.app
+        zip -r ../../release/vst2-arm64-osx.zip ../../bin/MusicGenVST.vst
+        zip -r ../../release/vst3-arm64-osx.zip ../../bin/MusicGenVST.vst3
     fi
 elif [ "$OS" = "Windows_NT" ]; then
     make 
